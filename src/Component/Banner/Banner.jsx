479964a735b3e0c41b6337d56a3f83af4558f9f3
Jishnuuu-k <krishnakumarodappanal@gmail>
2025-03-04 11:28:39 +0530

    updated

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import './Banner.css';
+import './banner.css';
 
 function Banner() {
   const [isVisible, setIsVisible] = useState(false);
